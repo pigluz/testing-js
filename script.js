0c481468af73b7da9a4ea233cefda49e21252691
pigluz <franciszeklatawiec@gmail.com>
2023-01-12 22:25:19 +0100

    add more on thing...

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ var nazwisko = document.getElementById("nazwisko");
 var wiek = document.getElementById("wiek");
 var miejsce = document.getElementById("mieszkanie");
 
-
+var submit = document.getElementById("submit");
+var reset = document.getElementById("reset");
 
 document.getElementById("submit").addEventListener("click", function () {
     alert("Hej " + imie.value + " " + nazwisko.value + "!\nMasz " + wiek.value + " lat! \nMieszkasz w " + miejsce.value);
 });
 
-
-
 function show() {
     document.getElementById("text").innerHTML = "Dziękujemy za udział w formularzu :)";
+}
+
+submit.onclick = function () {
+    reset.style.visibility = "visible";
 }
