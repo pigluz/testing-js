f66229ad286c20797a3668507b866d9b701f39fb
pigluz <franciszeklatawiec@gmail.com>
2023-01-14 11:16:47 +0100

    poorly fix bug

--- script.js
+++ script.js
@@ -5,15 +5,18 @@ var miejsce = document.getElementById("mieszkanie");
 
 var submit = document.getElementById("submit");
 var reset = document.getElementById("reset");
+var text = document.getElementById("text1")
 
-document.getElementById("submit").addEventListener("click", function () {
+document.getElementById("submit").addEventListener("click", function show() {
     alert("Hej " + imie.value + " " + nazwisko.value + "!\nMasz " + wiek.value + " lat! \nMieszkasz w " + miejsce.value);
 });
 
-function show() {
-    document.getElementById("text").innerHTML = "Dziękujemy za udział w formularzu :)";
-}
-
 submit.onclick = function () {
     reset.style.visibility = "visible";
+    text.style.visibility = "visible";
+}
+
+reset.onclick = function () {
+    reset.style.visibility = "hidden";
+    text.style.visibility = "hidden";
 }
